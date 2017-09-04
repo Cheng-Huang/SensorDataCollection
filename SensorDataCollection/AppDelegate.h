4969766e594bf58e54402e719c102ead8b140307
黄成 <387241256@qq.com>
2017-09-04 17:21:59 +0800

    Initial Commit

---
+++ SensorDataCollection/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SensorDataCollection
+//
+//  Created by 成 黄 on 2017/9/4.
+//  Copyright © 2017年 成 黄. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
